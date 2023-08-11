dacca2d9df17534b75e0c0af249ac26d8b05ffaa
Ayush Tyagi <80740475+ayushtyagi1901@users.noreply.github.com>
2023-08-11 12:26:30 +0530

    React Native Calculator with TypeScript and Dual Themes
    Introducing a sleek and modern calculator application, meticulously crafted using React Native and TypeScript. Designed with both functionality and aesthetics in mind, it offers users a choice between a minimalist light mode and a battery-friendly dark mode theme. Whether you're calculating daily expenses or solving complex mathematical problems, the intuitive interface ensures that tasks are handled with ease, catering to your visual preference at the touch of a button. Experience the seamless blend of precision, efficiency, and style, all encapsulated in this versatile tool.

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Switch, SafeAreaView } from "react-native";
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/styles/Colors";
+import Button from "./src/components/Button";
+import MyKeyboard from "./src/components/MyKeyboard";
 
 export default function App() {
+  const [theme, settheme] = useState("light");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "#000" }]
+        }
+      >
+        <StatusBar style="auto" />
+        <Switch style={{margin:20}}
+          value={theme === "light"}
+          onValueChange={() => settheme(theme === "light" ? "dark" : "light")}
+        />
+        <MyKeyboard/>
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: myColors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
